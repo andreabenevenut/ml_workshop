3f8042d57d4ed206415f7eb9cd69102caee2031a
andreabenevenuta <andrea@dataroots.io>
2023-05-04 17:25:05 +0200

    Adapting names in the pipeline script

--- ml/src/pipeline.py
+++ ml/src/pipeline.py
@@ -1,14 +1,12 @@
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
+import os
 
 try:
     credential = DefaultAzureCredential()
     credential.get_token("https://management.azure.com/.default")
 except Exception as ex:
     credential = InteractiveBrowserCredential()
-
-
-
 
 # Workspace
 subscription_id = "59a62e46-b799-4da2-8314-f56ef5acf82b"
@@ -23,13 +21,14 @@ ml_client = MLClient(
 
 
 
+# Data asset creation
 from azure.ai.ml.entities import Data
 from azure.ai.ml.constants import AssetTypes
 
 web_path = "https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls"
 
 credit_data = Data(
-    name="creditcard_defaults",
+    name="creditcard_defaults_andrea",
     path=web_path,
     type=AssetTypes.URI_FILE,
     description="Dataset for credit card defaults",
@@ -37,59 +36,27 @@ credit_data = Data(
     # version="1.0.0",
 )
 
-
 credit_data = ml_client.data.create_or_update(credit_data)
-print(
-    f"Dataset with name {credit_data.name} was registered to workspace, the dataset version is {credit_data.version}"
-)
+print(f"Dataset with name {credit_data.name} was registered to workspace, the dataset version is {credit_data.version}")
 
 
 
 
+# Compute cluster
 from azure.ai.ml.entities import AmlCompute
 
 cpu_compute_target = "aml-cluster"
 
-try:
-    # let's see if the compute target already exists
-    cpu_cluster = ml_client.compute.get(cpu_compute_target)
-    print(
-        f"You already have a cluster named {cpu_compute_target}, we'll reuse it as is."
-    )
-
-except Exception:
-    print("Creating a new cpu compute target...")
-
-    # Let's create the Azure ML compute object with the intended parameters
-    cpu_cluster = AmlCompute(
-        # Name assigned to the compute cluster
-        name="cpu-cluster",
-        # Azure ML Compute is the on-demand VM service
-        type="amlcompute",
-        # VM Family
-        size="STANDARD_DS3_V2",
-        # Minimum running nodes when there is no job running
-        min_instances=0,
-        # Nodes in cluster
-        max_instances=4,
-        # How many seconds will the node running after the job termination
-        idle_time_before_scale_down=180,
-        # Dedicated or LowPriority. The latter is cheaper but there is a chance of job termination
-        tier="Dedicated",
-    )
-
-    # Now, we pass the object to MLClient's create_or_update method
-    cpu_cluster = ml_client.begin_create_or_update(cpu_cluster)
-
-print(
-    f"AMLCompute with name {cpu_cluster.name} is created, the compute size is {cpu_cluster.size}"
-)
+cpu_cluster = ml_client.compute.get(cpu_compute_target)
+print( f"You already have a cluster named {cpu_compute_target}, we'll reuse it as is.")
 
 
+
+# Environment
 from azure.ai.ml.entities import Environment
+custom_env_name = "aml-scikit-learn_andrea"
 
-custom_env_name = "aml-scikit-learn"
-
+dependencies_dir = "./dependencies"
 pipeline_job_env = Environment(
     name=custom_env_name,
     description="Custom environment for Credit Card Defaults pipeline",
@@ -100,20 +67,19 @@ pipeline_job_env = Environment(
 )
 pipeline_job_env = ml_client.environments.create_or_update(pipeline_job_env)
 
-print(
-    f"Environment with name {pipeline_job_env.name} is registered to workspace, the environment version is {pipeline_job_env.version}"
-)
+print(f"Environment with name {pipeline_job_env.name} is registered to workspace, the environment version is {pipeline_job_env.version}")
 
 
+# Data prep component
 from azure.ai.ml import command
 from azure.ai.ml import Input, Output
 
+data_prep_src_dir = "./components/data_prep"
 data_prep_component = command(
-    name="data_prep_credit_defaults",
+    name="data_prep_credit_defaults_andrea",
     display_name="Data preparation for training",
     description="reads a .xl input, split the input to train and test",
     inputs={
-        # "data": Input(type="uri_folder"),
         "data": Input(type="uri_file"),
         "test_train_ratio": Input(type="number"),
     },
@@ -121,7 +87,6 @@ data_prep_component = command(
         train_data=Output(type="uri_folder", mode="rw_mount"),
         test_data=Output(type="uri_folder", mode="rw_mount"),
     ),
-    # The source folder of the component
     code=data_prep_src_dir,
     command="""python data_prep.py \
             --data ${{inputs.data}} --test_train_ratio ${{inputs.test_train_ratio}} \
@@ -130,38 +95,23 @@ data_prep_component = command(
     environment=f"{pipeline_job_env.name}:{pipeline_job_env.version}",
 )
 
-
-# Now we register the component to the workspace
 data_prep_component = ml_client.create_or_update(data_prep_component.component)
-
-# Create (register) the component in your workspace
-print(
-    f"Component {data_prep_component.name} with Version {data_prep_component.version} is registered"
-)
+print(f"Component {data_prep_component.name} with Version {data_prep_component.version} is registered")
 
 
 
-# importing the Component Package
+# Train Component
 from azure.ai.ml import load_component
 
-# Loading the component from the yml file
+train_src_dir = "./components/train"
 train_component = load_component(source=os.path.join(train_src_dir, "train.yml"))
-
-
-
-# Now we register the component to the workspace
 train_component = ml_client.create_or_update(train_component)
-
-# Create (register) the component in your workspace
-print(
-    f"Component {train_component.name} with Version {train_component.version} is registered"
-)
+print(f"Component {train_component.name} with Version {train_component.version} is registered")
 
 
 
-# the dsl decorator tells the sdk that we are defining an Azure ML pipeline
+# Pipeline definition
 from azure.ai.ml import dsl, Input, Output
-
 
 @dsl.pipeline(
     compute=cpu_compute_target,
@@ -173,13 +123,11 @@ def credit_defaults_pipeline(
     pipeline_job_learning_rate,
     pipeline_job_registered_model_name,
 ):
-    # using data_prep_function like a python call with its own inputs
     data_prep_job = data_prep_component(
         data=pipeline_job_data_input,
         test_train_ratio=pipeline_job_test_train_ratio,
     )
 
-    # using train_func like a python call with its own inputs
     train_job = train_component(
         train_data=data_prep_job.outputs.train_data,  # note: using outputs from previous step
         test_data=data_prep_job.outputs.test_data,  # note: using outputs from previous step
@@ -187,18 +135,12 @@ def credit_defaults_pipeline(
         registered_model_name=pipeline_job_registered_model_name,
     )
 
-    # a pipeline returns a dictionary of outputs
-    # keys will code for the pipeline output identifier
     return {
         "pipeline_job_train_data": data_prep_job.outputs.train_data,
         "pipeline_job_test_data": data_prep_job.outputs.test_data,
     }
 
-
-
-registered_model_name = "credit_defaults_model"
-
-# Let's instantiate the pipeline with the parameters of our choice
+registered_model_name = "credit_defaults_model_andrea"
 pipeline = credit_defaults_pipeline(
     pipeline_job_data_input=Input(type="uri_file", path=credit_data.path),
     pipeline_job_test_train_ratio=0.25,
@@ -207,14 +149,11 @@ pipeline = credit_defaults_pipeline(
 )
 
 
-
+# Submit the pipeline job
 import webbrowser
 
-# submit the pipeline job
 pipeline_job = ml_client.jobs.create_or_update(
     pipeline,
-    # Project's name
-    experiment_name="e2e_registered_components",
+    experiment_name="e2e_registered_components_andrea",
 )
-# open the pipeline in web browser
-webbrowser.open(pipeline_job.studio_url)
+#webbrowser.open(pipeline_job.studio_url)
